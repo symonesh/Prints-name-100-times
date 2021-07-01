9695b9e60884f91e1b4b2bef091224ea36b6dc38
symonesh <84563943+symonesh@users.noreply.github.com>
2021-07-01 16:36:53 +0545

    Create weekly worked hours and print the calculated weekly payment.c

---
+++ weekly worked hours and print the calculated weekly payment.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int hour,payment;
+  printf("Enter the number of hour worked on this week");
+  scanf("%d",&d);
+  if (x<42)
+  {
+    y=x*15;
+  printf("Your payment is:Rs%d",y)
+    }
+  else
+  {
+    y=42*15+(x-42)*20;
+  printf("your pay for this week is:Rs%d",y);
+  }
+  getch();
+      
+}
